2df452f38afb2f722af61d410384a5041efdce7b
Milinches <ibrogenius5165@gmail.com>
2021-02-02 22:42:55 +0100

    Created a database

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -11,7 +11,7 @@ class Articles(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        placement = ['-created_on']
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
