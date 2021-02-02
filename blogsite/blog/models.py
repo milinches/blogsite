ee73d2cbd36b31ac3d1daaca088c5388d7159880
Milinches <ibrogenius5165@gmail.com>
2021-02-02 22:13:45 +0100

    few changes

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    body = models.TextField()
+    updated_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        placement = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
