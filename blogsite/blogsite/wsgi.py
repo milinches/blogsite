e68ebb74fd2739426ea00ab300509b3a100806c5
Milinches <ibrogenius5165@gmail.com>
2021-02-04 07:05:52 +0100

    wsgi file modified

--- blogsite/blogsite/wsgi.py
+++ blogsite/blogsite/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogsite.settings')
+#
+# application = get_wsgi_application()
+
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogsite.settings')
+path = "/home/ashish/Documents/Django/blog/blog_project/"
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'blogsite.settings'
 
 application = get_wsgi_application()
